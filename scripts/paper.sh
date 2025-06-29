6a788a36db6b0401af37ccf1d80ff6ab1ee4f928
rhjddjdbc <hfufufdjdjdjd@protonmail.com>
2025-06-29 22:20:02 +0200

    Update paper.sh

--- scripts/paper.sh
+++ scripts/paper.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# Verzeichnis mit den Dateien
 DIR="/home/sascha/Wallpaper-live"
 
-# Zufällige Datei aus dem Verzeichnis wählen
 FILE=$(ls "$DIR" | shuf -n 1)
-
-# mpvpaper mit der zufällig gewählten Datei starten
 mpvpaper -o "--video-zoom=0.15 --mute=yes" ALL "$DIR/$FILE"
 
 
